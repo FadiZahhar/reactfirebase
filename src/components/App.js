0904c538d846a5e8fd910cef27eaade02b6dc620
fadizahhar <fadi.zahhar@ogilvy.com>
2019-08-20 13:04:53 +0300

    completing section 2 number 5 adding routing with react router 5

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 // import the broswer router from react rout dom
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import CreateLink from './Link/CreateLink';
+import Login from './Auth/Login';
+import ForgotPassword from './Auth/ForgotPassword';
+import SearchLinks from './Link/SearchLinks';
+import LinkList from './Link/LinkList';
+import LinkDetail from './Link/LinkDetail';
 
+/*
+*
+* the first route is the route page to redirect to the home page
+*/
 function App() {
   return (
     <BrowserRouter>
@@ -13,7 +23,7 @@ function App() {
         <Route path="/search" component={SearchLinks} />
         <Route path="/top" component={LinkList} />
         <Route path="/new/:page" component={LinkList} />
-        <Route path="/create" component={CreateLink} />
+        <Route path="/link/:linkId" component={LinkDetail} />
       </Switch>
     </BrowserRouter>
   );
