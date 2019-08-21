7c470a0d1cad5126d04d48c43a241c1397197f96
Fadi Nicolas Zahhar <fnzahhar@gmail.com>
2019-08-21 03:00:10 +0300

    creating the firebase project 4.10 create firebase project, setup auth resource

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAkFmde1FrtXar5VNi28J91ILWtaTtxvJ4",
+    authDomain: "hooknewsapp.firebaseapp.com",
+    databaseURL: "https://hooknewsapp.firebaseio.com",
+    projectId: "hooknewsapp",
+    storageBucket: "",
+    messagingSenderId: "252830617676",
+    appId: "1:252830617676:web:877beaa831d2a8d7"
+  };
+  export default firebaseConfig;
